8f680e632a7a2fc0a0c9377980baa9dbfe12477a
努力学IT的小陈 <140045095+Bratea@users.noreply.github.com>
2026-07-25 14:20:11 +0800

    auto: frontend/src/views/LayoutView.vue,

--- frontend/src/views/LayoutView.vue
+++ frontend/src/views/LayoutView.vue
@@ -64,7 +64,7 @@
       <!-- Page Content -->
       <div class="app-content">
         <router-view v-slot="{ Component }">
-          <keep-alive include="HomeView,SearchView,MyProfileView">
+          <keep-alive include="HomeView,SearchView,MyProfileView,SingerCenterView">
             <component :is="Component" />
           </keep-alive>
         </router-view>
